b3e36d83bc8e456df247e977a0b875b1915431c1
LabPOIO-04 <LabPOIO-04@vm-krim.local>
2026-05-16 16:31:15 +0200

    Projekt UML

---
+++ DefaultComponent/DefaultConfig/Default.h
@@ -0,0 +1,128 @@
+/*********************************************************************
+	Rhapsody	: 10.0.1 
+	Login		: LabPOIO-04
+	Component	: DefaultComponent 
+	Configuration 	: DefaultConfig
+	Model Element	: Default
+//!	Generated Date	: Sat, 16, May 2026  
+	File Path	: DefaultComponent/DefaultConfig/Default.h
+*********************************************************************/
+
+#ifndef Default_H
+#define Default_H
+
+//## auto_generated
+#include <oxf/oxf.h>
+//## auto_generated
+#include <aom/aom.h>
+//## auto_generated
+#include <oxf/event.h>
+//## auto_generated
+class BramaWjaz;
+
+//## auto_generated
+class Odbiornik;
+
+//#[ ignore
+#define otwieranie_Default_id 18601
+
+#define zamykanie_Default_id 18602
+
+#define click_Default_id 18603
+//#]
+
+//## package Default
+
+
+
+//## event otwieranie()
+class otwieranie : public OMEvent {
+    ////    Friends    ////
+    
+public :
+
+#ifdef _OMINSTRUMENT
+    friend class OMAnimatedotwieranie;
+#endif // _OMINSTRUMENT
+
+    ////    Constructors and destructors    ////
+    
+    //## auto_generated
+    otwieranie();
+    
+    ////    Framework operations    ////
+    
+    //## statechart_method
+    virtual bool isTypeOf(const short id) const;
+};
+
+#ifdef _OMINSTRUMENT
+//#[ ignore
+class OMAnimatedotwieranie : virtual public AOMEvent {
+    DECLARE_META_EVENT(otwieranie)
+};
+//#]
+#endif // _OMINSTRUMENT
+
+//## event zamykanie()
+class zamykanie : public OMEvent {
+    ////    Friends    ////
+    
+public :
+
+#ifdef _OMINSTRUMENT
+    friend class OMAnimatedzamykanie;
+#endif // _OMINSTRUMENT
+
+    ////    Constructors and destructors    ////
+    
+    //## auto_generated
+    zamykanie();
+    
+    ////    Framework operations    ////
+    
+    //## statechart_method
+    virtual bool isTypeOf(const short id) const;
+};
+
+#ifdef _OMINSTRUMENT
+//#[ ignore
+class OMAnimatedzamykanie : virtual public AOMEvent {
+    DECLARE_META_EVENT(zamykanie)
+};
+//#]
+#endif // _OMINSTRUMENT
+
+//## event click()
+class click : public OMEvent {
+    ////    Friends    ////
+    
+public :
+
+#ifdef _OMINSTRUMENT
+    friend class OMAnimatedclick;
+#endif // _OMINSTRUMENT
+
+    ////    Constructors and destructors    ////
+    
+    //## auto_generated
+    click();
+    
+    ////    Framework operations    ////
+    
+    //## statechart_method
+    virtual bool isTypeOf(const short id) const;
+};
+
+#ifdef _OMINSTRUMENT
+//#[ ignore
+class OMAnimatedclick : virtual public AOMEvent {
+    DECLARE_META_EVENT(click)
+};
+//#]
+#endif // _OMINSTRUMENT
+
+#endif
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/Default.h
+*********************************************************************/
